1a3aed6b7293280cf32dd0bfc155fef27ba3ea5e
Mateusz Sulima <mateusz.sulima@allegro.pl>
2015-02-22 21:13:00 +0100

    - batch

--- src/main/scala/pl/msulima/redis/benchmark/repository/JedisAkkaBatchRepositoryComponent.scala
+++ src/main/scala/pl/msulima/redis/benchmark/repository/JedisAkkaBatchRepositoryComponent.scala
@@ -1,133 +1,119 @@
 package pl.msulima.redis.benchmark.repository
 
 import java.util
-import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.locks.ReentrantLock
-import java.util.concurrent.{Executors, LinkedBlockingQueue, TimeUnit}
+import java.util.concurrent.{ConcurrentLinkedQueue, Executors, LinkedBlockingQueue, TimeUnit}
+import java.util.{Timer, TimerTask}
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.ActorSystem
 import akka.util.Timeout
-import pl.msulima.redis.benchmark.repository.PipeliningActor.{Get, Request, Set, Tick}
-import redis.clients.jedis.{JedisPool, Response}
+import pl.msulima.redis.benchmark.repository.PipeliningActor.{Get, Request, Set}
+import redis.clients.jedis.{JedisPool, Pipeline, Response}
 
+import scala.annotation.tailrec
 import scala.collection.JavaConversions._
-import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
 trait JedisAkkaBatchRepositoryComponent {
 
   class JedisAkkaBatchRepository(system: ActorSystem)(implicit ec: ExecutionContext) extends Repository {
-    private val props = Props(new PipeliningActor)
-    private val actor = system.actorOf(props)
     private implicit val timeout = Timeout(1, TimeUnit.SECONDS)
+    private val requests = new ConcurrentLinkedQueue[(Promise[_], Request)]()
+    private val batches = new LinkedBlockingQueue[util.LinkedList[(Promise[_], Request)]]()
+
+    private val sleepTime = 10
+    private val batchSize = 5000
+    private val pool = new JedisPool("localhost")
+    private val lock = new ReentrantLock()
+
+    private val redisEc = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(5))
+    private val redisSchedulerEc = new Timer()
 
     override def mget(keys: Seq[String]): Future[Seq[Payload]] = {
       val promisedStrings = Promise[Seq[Payload]]()
-      actor !(promisedStrings, Get(keys))
+      requests.offer(promisedStrings -> Get(keys))
       promisedStrings.future
     }
 
     override def mset(keys: Seq[(String, Payload)]) = {
       val promisedStrings = Promise[Seq[(String, Payload)]]()
-      actor !(promisedStrings, Set(keys))
+      requests.offer(promisedStrings -> Set(keys))
       promisedStrings.future
     }
-  }
 
-  class PipeliningActor extends Actor {
+    class Batcher extends TimerTask {
+      override def run(): Unit = {
+        readElements()
+      }
 
-    private val pool = new JedisPool("localhost")
-
-    private val requests = new util.concurrent.ConcurrentLinkedQueue[(Promise[_], Request)]
-    private implicit val ec = context.dispatcher
-
-    override def preStart() = {
-      context.system.scheduler.schedule(0.milliseconds, 20.milliseconds, self, Tick)
-    }
-
-    private val redisEc = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
-    private val queueSize = new AtomicInteger(0)
-    val batchSize = new AtomicInteger(500)
-    val exceeded = new AtomicInteger(0)
-
-    val decThreshold = 5
-    val incThreshold = 15
-
-    override def receive: Receive = {
-      case request: (Promise[_], Request) =>
-        requests.add(request)
-        if (requests.size == batchSize.get) {
-          runQuery()
+      @tailrec
+      private def readElements(): Unit = {
+        val elements = new util.LinkedList[(Promise[_], Request)]()
+        var element = requests.poll()
+        while (element != null && elements.size() < batchSize) {
+          elements += element
+          element = requests.poll()
         }
-      case Tick =>
-        runQuery()
-        if (exceeded.get() > 10) {
-          exceeded.set(10)
-        } else if (exceeded.get() < -10) {
-          exceeded.set(-10)
-        }
-    }
 
-    private val queryQueue = new LinkedBlockingQueue[Vector[(Promise[_], Request)]]()
-    private val lock = new ReentrantLock()
-
-    private def runQuery() = {
-      val requestsCopy = Vector.empty ++ requests
-
-      if (requestsCopy.size > 0) {
-        requests.clear()
-        queryQueue.add(requestsCopy)
-
-        val currentQueueSize = queueSize.getAndIncrement
-        if (currentQueueSize > incThreshold) {
-          if (exceeded.incrementAndGet() > 10 && batchSize.get < 5000) {
-            val newBatchSize = batchSize.addAndGet(10)
-            println((" " * (currentQueueSize / 3)) + currentQueueSize, newBatchSize)
-            exceeded.set(0)
+        if (elements.size() == batchSize) {
+          if (element != null) {
+            elements += element
           }
-        } else if (currentQueueSize < decThreshold && batchSize.get > 50) {
-          if (exceeded.decrementAndGet() < -10) {
-            val newBatchSize = batchSize.addAndGet(-10)
-            println((" " * (currentQueueSize / 3)) + currentQueueSize, newBatchSize)
-            exceeded.set(0)
+          print(".")
+          batches.offer(elements)
+          readElements()
+        } else {
+          if (elements.size() > 0) {
+            batches.offer(elements)
           }
+          redisSchedulerEc.schedule(new Batcher, sleepTime)
         }
-        redisEc.execute(new RedisThread)
       }
     }
 
     class RedisThread extends Runnable {
       override def run(): Unit = {
-        lock.lock()
-        val (p, conn) = try {
-          val requestsCopy = queryQueue.poll()
+        while (true) {
+          lock.lock()
+          val p = try {
+            val requestsCopy = batches.take()
 
-          val connection = pool.getResource
-          val jedis = connection.pipelined()
-          val pipelined = requestsCopy.map {
-            case (replyTo, Get(keys)) =>
-              (replyTo, Get(keys), jedis.mget(keys.map(_.getBytes): _*))
-            case (replyTo, Set(keys)) =>
-              (replyTo, Set(keys), jedis.mset(keys.flatMap(k => Seq(k._1.getBytes, k._2)): _*))
+            val connection = pool.getResource
+            val jedis = connection.pipelined()
+            val pipelined = makeRequest(jedis, requestsCopy)
+            jedis.sync()
+            connection.close()
+            pipelined
+          } finally {
+            lock.unlock()
           }
-          jedis.sync()
-          (pipelined, connection)
-        } finally {
-          lock.unlock()
-        }
 
-        p.foreach {
-          case (promise, Get(_), response) =>
-            val values = response.asInstanceOf[Response[util.List[Array[Byte]]]].get.toSeq.filterNot(_ == null)
-            promise.asInstanceOf[Promise[Seq[Array[Byte]]]].success(values)
-          case (promise, Set(keys), _) =>
-            promise.asInstanceOf[Promise[Seq[(String, Array[Byte])]]].success(keys)
+          p.foreach {
+            case (promise, Get(_), response) =>
+              val values = response.asInstanceOf[Response[util.List[Array[Byte]]]].get.toSeq.filterNot(_ == null)
+              promise.asInstanceOf[Promise[Seq[Array[Byte]]]].success(values)
+            case (promise, Set(keys), _) =>
+              promise.asInstanceOf[Promise[Seq[(String, Array[Byte])]]].success(keys)
+          }
         }
-        queueSize.decrementAndGet()
-        conn.close()
+      }
+
+      private def makeRequest(jedis: Pipeline, elements: util.List[(Promise[_], Request)]) = {
+        elements.map {
+          case (replyTo, Get(keys)) =>
+            (replyTo, Get(keys), jedis.mget(keys.map(_.getBytes): _*))
+          case (replyTo, Set(keys)) =>
+            (replyTo, Set(keys), jedis.mset(keys.flatMap(k => Seq(k._1.getBytes, k._2)): _*))
+        }
       }
     }
 
+    for (i <- 1 to 5) {
+      val thread = new Thread(new RedisThread)
+      thread.setPriority(10)
+      thread.start()
+    }
+    redisSchedulerEc.schedule(new Batcher, 10)
   }
 
 }
