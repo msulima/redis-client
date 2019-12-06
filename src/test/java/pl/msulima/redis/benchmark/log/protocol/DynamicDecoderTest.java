a92eda634f5231b1599e7f167b685f708b37a11c
ms <sulima.mateusz@gmail.com>
2019-12-06 22:54:32 +0100

    Log client - add quickcheck

--- src/test/java/pl/msulima/redis/benchmark/log/protocol/DynamicDecoderTest.java
+++ src/test/java/pl/msulima/redis/benchmark/log/protocol/DynamicDecoderTest.java
@@ -1,7 +1,14 @@
 package pl.msulima.redis.benchmark.log.protocol;
 
 import com.google.common.base.Charsets;
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -10,22 +17,22 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitQuickcheck.class)
 public class DynamicDecoderTest {
 
     private static final int BUFFER_SIZE = 64;
     private final DynamicDecoder reader = new DynamicDecoder();
 
-    @Test
-    public void testSimpleString() {
+    @Property
+    public void testSimpleString(@From(SimpleStringGenerator.class) String text) {
         // given
-        String ok = "OK";
-        ByteBuffer in = createByteBuffer("+" + ok + "\r\n");
+        ByteBuffer in = createByteBuffer("+" + text + "\r\n");
 
         // when
         reader.read(in);
 
         // then
-        assertThat(reader.response).isEqualTo(Response.simpleString(ok));
+        assertThat(reader.response).isEqualTo(Response.simpleString(text));
     }
 
     @Test
@@ -102,5 +109,30 @@ public class DynamicDecoderTest {
             expectedResponses.add(Response.bulkString(bytes));
         }
         assertThat(responses).isEqualTo(expectedResponses);
+    }
+
+    public static class SimpleStringGenerator extends Generator<String> {
+
+        private static final String LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
+        private static final String UPPERCASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static final String NUMBERS = "0123456789";
+        private static final String SPECIAL_CHARS = ".-\\;:_@[]^/|}{";
+        private static final String ALL_MY_CHARS = LOWERCASE_CHARS + UPPERCASE_CHARS + NUMBERS + SPECIAL_CHARS;
+        private static final int CAPACITY = BUFFER_SIZE - 3;
+
+        public SimpleStringGenerator() {
+            super(String.class);
+        }
+
+        @Override
+        public String generate(SourceOfRandomness r, GenerationStatus s) {
+            int capacity = r.nextInt(CAPACITY - 1) + 1;
+            StringBuilder sb = new StringBuilder(capacity);
+            for (int i = 0; i < capacity; i++) {
+                int randomIndex = r.nextInt(ALL_MY_CHARS.length());
+                sb.append(ALL_MY_CHARS.charAt(randomIndex));
+            }
+            return sb.toString();
+        }
     }
 }
