4dbe5143a7a460dc187b8b271c9511a6ad3f2eae
MateuszSulimaRelayr <mateusz.sulima@relayr.io>
2020-06-26 19:12:01 +0200

    Log client

--- src/main/scripts/start-redis.sh
+++ src/main/scripts/start-redis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-redis-5.0.7/src/redis-server --save "" --appendonly no --bind 0.0.0.0 --port ${1:-6379}
+redis-6.0.5/src/redis-server --save "" --appendonly no --bind 0.0.0.0 --port ${1:-6379}
